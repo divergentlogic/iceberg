fbeb1eff1b35c7e812a64002789cd78e8e9112fb
Christopher Durtschi <cdurtschi@divergentlogic.com>
2009-12-09 23:52:48 -0700

    Don't misuse method aliasing

--- lib/mixins/external_layout.rb
+++ lib/mixins/external_layout.rb
@@ -4,15 +4,13 @@ module Iceberg
       def self.included(base)
         base.class_eval do
           private
-      
-          def render_with_external_layout(engine, data, options={}, locals={}, &block)
+          
+          def render(engine, data, options={}, locals={}, &block)
             unless options[:layout]
               options.merge!({:layout => self.options.external_layout}) if self.options.respond_to?(:external_layout) && self.options.external_layout
             end
-            render_without_external_layout(engine, data, options, locals, &block)
+            super(engine, data, options, locals, &block)
           end
-          alias_method :render_without_external_layout, :render
-          alias_method :render, :render_with_external_layout
         end
       end
     end
