dc5a98d6b43efe7d6e19b542f71de4d0d45de48d
robotapocalypse <pherph@gmail.com>
2010-01-12 12:21:12 -0700

    Move factories to single file

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,3 +2,8 @@ Factory.define :forum, :class => Iceberg::Forum do |f|
   f.title "This is a General Forum"
   f.description "Talk about general topics in this forum"
 end
+
+Factory.define :topic, :class => Iceberg::Topic do |f|
+  f.title "Let's talk about something"
+  f.message "What do you guys want to talk about?"
+end
--- spec/factories/topic_factory.rb
+++
@@ -1,4 +0,0 @@
-Factory.define :topic, :class => Iceberg::Topic do |f|
-  f.title "Let's talk about something"
-  f.message "What do you guys want to talk about?"
-end
